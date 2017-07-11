591371083f0fef14f73382e08a0632aefcf3a2c2
Ian Tobin <imt1195@gmail.com>
2017-07-11 17:00:34 -0400

    Create Cyphers.h
    function prototypes for encryption and decryption functions

---
+++ cpp/Cyphers.h
@@ -0,0 +1,14 @@
+#ifndef Cyphers
+#define Cyphers
+
+#include <iostream>
+
+using namespace std;
+
+char *Ceasar_Encrypt();
+char *Ceasar_Decrypt();
+
+char *Vigenere_Encrypt();
+char *Vigenere_Decrypt();
+
+#endif // !Chyphers

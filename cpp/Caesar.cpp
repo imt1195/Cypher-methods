b0a16111f671e7eaa80d48bd467b4876e0019b44
Ian Tobin <imt1195@gmail.com>
2017-07-11 16:59:40 -0400

    Create Caesar.cpp
    Caesar cypher encryption and decryption functions

---
+++ cpp/Caesar.cpp
@@ -0,0 +1,65 @@
+#include "Chyphers.h"
+
+char message[1000];
+
+char *Ceasar_Encrypt() {
+	char ch;
+	int key;
+
+	cout << "Enter a message to encrypt: " << endl;
+	cin >> message;
+	cout << "Enter key (number): " << endl;
+	cin >> key;
+
+	for (int i = 0; message[i] != '\0'; ++i) {
+		ch = message[i];
+
+		if (ch >= 'a' && ch <= 'z') {
+			ch = ch + key;
+			if (ch > 'z') {
+				ch = ch - 'z' + 'a' - 1;
+			}
+			message[i] = ch;
+		}else if (ch >= 'A' && ch <= 'Z') {
+			ch = ch + key;
+			if (ch > 'Z') {
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			message[i] = ch;
+		}
+	}
+	return message;
+}
+
+char *Ceasar_Decrypt() {
+	char ch;
+	int key;
+
+	cout << "Enter a message to decrypt: " << endl;
+	cin >> message;
+	cout << "Enter key (number): " << endl;
+	cin >> key;
+
+	for (int i = 0; message[i] != '\0'; ++i) {
+		ch = message[i];
+
+		if (ch >= 'a' && ch <= 'z') {
+			ch = ch - key;
+
+			if (ch < 'a') {
+				ch = ch + 'z' - 'a' + 1;
+			}
+
+			message[i] = ch;
+		}else if (ch >= 'A' && ch <= 'Z') {
+			ch = ch - key;
+
+			if (ch < 'A') {
+				ch = ch + 'Z' - 'A' + 1;
+			}
+
+			message[i] = ch;
+		}
+	}
+	return message;
+}
